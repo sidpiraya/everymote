5052efac9a8e9321890440eb3073b433969da452
Michael Nilsson <michael.nilsson@spelkultur.com>
2012-04-25 16:02:58 +0200

    first code for spotify app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 everymote-spotify-app
 =====================
+
+This is at least at this time a prototype to test if it would be a nice user experience to hock spotify onto everymote.com
+
+
---
+++ everymote.js
@@ -0,0 +1,34 @@
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+var player = models.player;
+
+exports.init = init;
+exports.next = next_song;
+function init() {
+    updatePageWithTrackDetails();
+
+    player.observe(models.EVENT.CHANGE, function (e) {
+
+	// Only update the page if the track changed
+	if (e.data.curtrack == true) {
+        	updatePageWithTrackDetails();
+        }
+    });
+}
+        
+
+function next_song(){
+	player.next();
+}
+
+function updatePageWithTrackDetails() {
+	var header = document.getElementById("header");
+        var playerTrackInfo = player.track;
+        
+        if (playerTrackInfo == null) {
+        	header.innerText = "Nothing playing!";
+        } else {
+        	var track = playerTrackInfo.data;
+                header.innerHTML = track.name + " on the album " + track.album.name + " by " + track.album.artist.name + ".";
+        }
+}
